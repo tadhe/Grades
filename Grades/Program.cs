d598d65a6e4bdaceccaf2457f522799b3a480e4f
tadhe <tadeusz.hreczecha@gmail.com>
2018-09-12 12:11:09 +0200

    Add basic functionality to Main

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -15,6 +15,11 @@ namespace Grades
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
         }
     }
 }
