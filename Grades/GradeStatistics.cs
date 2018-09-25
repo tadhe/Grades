9a938feb214136e0d085d33ab5eb7a8caf9c0f3b
tadhe <tadeusz.hreczecha@gmail.com>
2018-09-25 22:36:59 +0200

    Add description - switch m6-03

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -43,6 +43,34 @@ namespace Grades
             }
         }
 
+        public string Description
+        {
+            get
+            {
+                string result;
+                switch (LetterGrade)
+                {
+                    case "A":
+                        result = "Excelent";
+                        break;
+                    case "B":
+                        result = "Good";
+                        break;
+                    case "C":
+                        result = "Average";
+                        break;
+                    case "D":
+                        result = "Below average";
+                        break;
+                    default:
+                        result = "Failing";
+                        break;
+
+                }
+                return result;
+            }
+        }
+
         public float AverageGrade;
         public float HighestGrade;
         public float LowestGrade;
